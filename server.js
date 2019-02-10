18df96e0c7f80b25a0b3aad191cad15eaf1381a1
Erick Campuzano <erick.campuzano@outlook.com>
2019-02-09 19:03:02 -0600

    Fixed Favicon

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var serveStatic = require('serve-static');
 var favicon = require("express-favicon");
 
 app = express();
-app.use(favicon(__dirname+"/dist/favicon.ico"));
 app.use(serveStatic(__dirname + "/dist"));
+app.use(favicon(__dirname + "/dist/favicon.ico"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started' + port);
