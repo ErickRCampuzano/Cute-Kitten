19831eeea481f0edfdb4e0b2f548aa7e44b2c6f9
Erick Campuzano <erick.campuzano@outlook.com>
2019-02-09 19:00:18 -0600

    Added favicon for express

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+var favicon = require("express-favicon");
 
 app = express();
+app.use(favicon(__dirname+"/dist/favicon.ico"));
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
